b25f4c7ebc3d558cfb877a2d13cf83b15b157a8e
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-06-02 19:47:10 +0000

    Cards generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,32 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//----------------------------------------
+
+function getRandomNum() {
+  return Math.floor(Math.random() * (12 - 1)) + 1;
+}
+
+const cards = ["♦", "♥", "♠", "♣"];
+
+function getRandomCard(arr) {
+  return arr[Math.floor(Math.random() * (arr.length - 0)) + 0];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = getRandomNum();
+  let signo = document.querySelectorAll(".sign");
+  var lala = getRandomCard(cards);
+  for (var i = 0; i < signo.length; i++) {
+    signo[i].innerHTML = lala;
+  }
+
+  document.getElementById("boton").addEventListener("click", function() {
+    document.querySelector(".number").innerHTML = getRandomNum();
+    let signo = document.querySelectorAll(".sign");
+    var lala = getRandomCard(cards);
+    for (var i = 0; i < signo.length; i++) {
+      signo[i].innerHTML = lala;
+    }
+  });
 };
