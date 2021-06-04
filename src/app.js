89511cc233a901ef15604fede5ae1a8dd6ecdffd
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-06-04 16:44:43 +0000

    updated Card generator

--- src/app.js
+++ src/app.js
@@ -6,30 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 //----------------------------------------
 
+function CambioColor(element, x) {
+  if (element == "♦" || element == "♥") {
+    x.classList.remove("negro");
+    x.classList.add("rojo");
+  } else {
+    x.classList.remove("rojo");
+    x.classList.add("negro");
+  }
+}
+
 function getRandomNum() {
   return Math.floor(Math.random() * (12 - 1)) + 1;
 }
 
-const cards = ["♦", "♥", "♠", "♣"];
+const cartas = { diamante: "♦", corazones: "♥", picas: "♠", trebol: "♣" };
 
-function getRandomCard(arr) {
-  return arr[Math.floor(Math.random() * (arr.length - 0)) + 0];
-}
+var randomProperty = function(obj) {
+  var keys = Object.keys(obj);
+  var result = obj[keys[(keys.length * Math.random()) << 0]];
+  return result;
+};
 
 window.onload = function() {
   document.querySelector(".number").innerHTML = getRandomNum();
   let signo = document.querySelectorAll(".sign");
-  var lala = getRandomCard(cards);
+  var cards = randomProperty(cartas);
   for (var i = 0; i < signo.length; i++) {
-    signo[i].innerHTML = lala;
+    signo[i].innerHTML = cards;
+    CambioColor(cards, signo[i]);
   }
 
   document.getElementById("boton").addEventListener("click", function() {
     document.querySelector(".number").innerHTML = getRandomNum();
     let signo = document.querySelectorAll(".sign");
-    var lala = getRandomCard(cards);
+    var cards = randomProperty(cartas);
     for (var i = 0; i < signo.length; i++) {
-      signo[i].innerHTML = lala;
+      signo[i].innerHTML = cards;
+      CambioColor(cards, signo[i]);
     }
   });
 };
